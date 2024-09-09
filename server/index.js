b0fb9e41eadd01dba173a38168fbd93b77509795
akcodec <ankitkumar2000saini@gmail.com>
2024-09-09 23:28:17 +0530

    sight changes 3

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
 app.get("/",(req,res)=>{
-  res.send("hello")
+  res.json("hello");
 })
 
 app.use("/api/user/", UserRoutes);
