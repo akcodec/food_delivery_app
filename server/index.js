a77327093fa9f57aa743dcfddecf9ebc159eb035
akcodec <ankitkumar2000saini@gmail.com>
2024-09-09 23:10:22 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import FoodRoutes from "./routes/Food.js";
 env.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: {"https://deployfood-delivery-app-dusky-eight.vercel.app"},
+  methods: ["POST","GET"],
+  credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
